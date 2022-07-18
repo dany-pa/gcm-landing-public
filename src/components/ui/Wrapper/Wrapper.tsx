9907a3d2c00c8daf92596f14cf8c274021e9ba8c
Patskin Daniil <daniil.patskin@gmail.com>
2022-07-19 01:16:10 +0300

    Подкорректировал враппер

--- src/components/ui/Wrapper/Wrapper.tsx
+++ src/components/ui/Wrapper/Wrapper.tsx
@@ -1,22 +1,30 @@
 import { css, SerializedStyles } from '@emotion/react';
 import { FC, PropsWithChildren } from 'react';
-import { BREAKPOINT_MOBILE, BREAKPOINT_TABLET } from '../../../const/breakpoints';
+import { BREAKPOINT_LAPTOP, BREAKPOINT_MOBILE, BREAKPOINT_TABLET } from '../../../const/breakpoints';
 interface Props {
     style?: SerializedStyles;
 }
 
 export const Wrapper: FC<PropsWithChildren<Props>> = ({ style, children }) => {
     const wrapperStyle = css({
-        paddingLeft: 112,
-        paddingRight: 112,
+        maxWidth: 1070,
+        marginLeft: 'calc((100vw - 1070px) / 2)',
+        marginRight: 'calc((100vw - 1070px) / 2)',
+        [`@media (max-width: ${BREAKPOINT_LAPTOP}px)`]: {
+            maxWidth: 950,
+            marginLeft: 'calc((100vw - 950px) / 2)',
+            marginRight: 'calc((100vw - 950px) / 2)',
+        },
         [`@media (max-width: ${BREAKPOINT_TABLET}px)`]: {
-            paddingLeft: 100,
-            paddingRight: 100,
+            maxWidth: 550,
+            marginLeft: 'calc((100vw - 550px) / 2)',
+            marginRight: 'calc((100vw - 550px) / 2)',
         },
 
         [`@media (max-width: ${BREAKPOINT_MOBILE}px)`]: {
-            paddingLeft: 35,
-            paddingRight: 35,
+            maxWidth: 400,
+            marginLeft: 'calc((100vw - 400px) / 2)',
+            marginRight: 'calc((100vw - 400px) / 2)',
         },
     });
 
