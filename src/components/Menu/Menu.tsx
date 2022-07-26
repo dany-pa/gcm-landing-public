d77732e8d1809344add7ea132e780e3c104e9074
Patskin Daniil <daniil.patskin@gmail.com>
2022-07-26 23:31:38 +0300

    Добавил класс и лого в меню

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,15 +1,26 @@
 import { FC } from 'react';
+import { COLOR_SECONDARY } from '../../const/colors';
 import { MenuLinkType } from '../../types/general';
+import { LogoIcon } from '../icons';
 import { MenuLink } from '../ui/MenuLink';
-import { navStyle } from './styles';
+import { logoIconWrapperStyle, navStyle } from './styles';
 
 interface Props {
     links: MenuLinkType[];
+    isShowLogo?: boolean;
 }
 
-export const Menu: FC<Props> = ({ links }) => {
+export const Menu: FC<Props> = ({ links, isShowLogo = false }) => {
     return (
-        <nav css={navStyle}>
+        <nav
+            css={navStyle}
+            className="nav"
+        >
+            {isShowLogo && (
+                <div css={logoIconWrapperStyle}>
+                    <LogoIcon color={COLOR_SECONDARY} />
+                </div>
+            )}
             {links.map((link) => {
                 return (
                     <MenuLink
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,7 +1,17 @@
 import { css } from '@emotion/react';
+import { BREAKPOINT_TABLET } from '../../const/breakpoints';
 
 export const navStyle = css({
     width: '100%',
     display: 'flex',
     justifyContent: 'space-between',
+});
+
+export const logoIconWrapperStyle = css({
+    width: 128,
+    height: 87,
+
+    [`@media (max-width: ${BREAKPOINT_TABLET}px)`]: {
+        display: 'none',
+    },
 });
